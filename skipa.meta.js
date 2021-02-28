c019faefddfa6051e7f045c86dd078f1e8f58bad
Yarooze <yarooze@yahoo.de>
2021-02-28 16:07:14 +0100

    init

---
+++ skipa.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Skipa
+// namespace    https://github.com/yarooze/skipa.ls
+// @version     0.0.0
+// @description Skip some stuff
+// @author      Yarooze
+// icon         https://github.com/yarooze/skipa.ls/raw/master/Icon.png
+// updateURL    https://github.com/yarooze/skipa.ls/raw/master/skipa.meta.js
+// downloadURL  https://github.com/yarooze/skipa.ls/raw/master/skipa.user.js
+// @match        https://www.youtube.com/*
+// @grant       none
+// @noframes
+// ==/UserScript==
